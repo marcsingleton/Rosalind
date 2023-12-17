8d7a1dc54046324af72c859018bafe42f3132a62
Marc Singleton <marcsingleton@berkeley.edu>
2023-12-17 15:50:36 -0800

    Added GREP.py

---
+++ CSET.py
@@ -0,0 +1,51 @@
+"""Fixing an Inconsistent Character Set.
+
+Given: An inconsistent character table C on at most 100 taxa.
+
+Return: A submatrix of C′ representing a consistent character table on the same taxa and formed by deleting a single row
+of C. (If multiple solutions exist, you may return any one.)
+"""
+
+from itertools import combinations, product
+
+
+def get_splits(characters):
+    split0, split1 = set(), set()
+    for index, character in enumerate(characters):
+        if character == '0':
+            split0.add(index)
+        else:
+            split1.add(index)
+    return split0, split1
+
+
+data = """\
+100001
+000110
+111000
+100111
+"""
+
+table = data.rstrip('\n').split('\n')
+
+conflicts = []
+for characters1, characters2 in combinations(table, 2):
+    splits1 = get_splits(characters1)
+    splits2 = get_splits(characters2)
+    intersections = [sA & sB for sA, sB in product(splits1, splits2)]
+    if all(intersections):  # All are nonempty
+        conflicts.append((characters1, characters2))
+
+counts = {}
+for characters1, characters2 in conflicts:
+    counts[characters1] = counts.get(characters1, 0) + 1
+    counts[characters2] = counts.get(characters2, 0) + 1
+
+new_table = []
+conflict = max(counts, key=lambda x: counts[x])
+for character in table:
+    if character != conflict:
+        new_table.append(character)
+
+for character in new_table:
+    print(character)
---
+++ GREP.py
@@ -0,0 +1,60 @@
+"""Genome Assembly with Perfect Coverage and Repeats.
+
+Given: A list Sk+1 of error-free DNA (k+1)-mers (k ≤ 5) taken from the same strand of a circular chromosome (of
+length ≤ 50).
+
+Return: All circular strings assembled by complete cycles in the de Bruijn graph Bk of Sk+1. The strings may be given in
+any order, but each one should begin with the first (k+1)-mer provided in the input.
+"""
+
+from collections import Counter
+
+data = """\
+CAG
+AGT
+GTT
+TTT
+TTG
+TGG
+GGC
+GCG
+CGT
+GTT
+TTC
+TCA
+CAA
+AAT
+ATT
+TTC
+TCA
+"""
+
+seqs = data.rstrip('\n').split('\n')
+
+graph = {}
+for seq in seqs:
+    prefix, suffix = seq[:-1], seq[1:]
+    if prefix not in graph:
+        graph[prefix] = {suffix}
+    else:
+        graph[prefix].add(suffix)
+    if suffix not in graph:
+        graph[suffix] = set()
+
+prefix0 = seqs[0][:-1]
+prefix = seqs[0][1:]
+reads = Counter(seqs[1:])
+stack = [(prefix, [prefix[-1]], reads)]
+for prefix, chromosome, reads in stack:
+    if sum(reads.values()) == 0:
+        chromosome = ''.join(chromosome)
+        print(chromosome[-len(prefix0):] + chromosome[:-len(prefix0)])
+        continue
+    for suffix in graph[prefix]:
+        seq = prefix + suffix[-1]
+        if reads[seq] > 0:
+            new_reads = reads.copy()
+            new_reads[seq] -= 1
+            new_chromosome = chromosome.copy()
+            new_chromosome.append(suffix[-1])
+            stack.append((suffix, new_chromosome, new_reads))
