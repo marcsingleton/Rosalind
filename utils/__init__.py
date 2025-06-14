cf3879df004179eb296a1e98e6eccca7f92e0bf4
Marc Singleton <marcsingleton@berkeley.edu>
2025-06-13 22:44:19 -0700

    Created parse_fasta function

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 """Utility functions written for Rosalind problems."""
 
-from .fasta import read_fasta
+from .fasta import parse_fasta
 from .tree import read_newick
 from .sequence import reverse_complement
--- utils/fasta.py
+++ utils/fasta.py
@@ -1,6 +1,25 @@
 """Utility functions for manipulating FASTA files."""
 
 
+def parse_fasta(file):
+    """Read FASTA from file object and return list of headers and sequences.
+    
+    Refer to read_fasta for details.
+    """
+    line = file.readline()
+    while line:
+        if line.startswith('>'):
+            header = line.rstrip()
+            line = file.readline()
+
+        seqlines = []
+        while line and not line.startswith('>'):
+            seqlines.append(line.rstrip())
+            line = file.readline()
+        seq = ''.join(seqlines)
+        yield header,seq
+
+
 def read_fasta(path):
     """Read FASTA file at path and return list of headers and sequences.
 
@@ -16,15 +35,4 @@ def read_fasta(path):
         element is the corresponding sequence.
     """
     with open(path) as file:
-        line = file.readline()
-        while line:
-            if line.startswith('>'):
-                header = line.rstrip()
-                line = file.readline()
-
-            seqlines = []
-            while line and not line.startswith('>'):
-                seqlines.append(line.rstrip())
-                line = file.readline()
-            seq = ''.join(seqlines)
-            yield header, seq
+        yield from parse_fasta(file)
